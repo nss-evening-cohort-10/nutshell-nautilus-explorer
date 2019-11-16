ae8abdf2244bc41bf8bfd3c8da63d73eeda79c06
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-11-16 13:57:50 -0600

    commiting to view pull request

---
+++ src/javascripts/helpers/speciesData.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import apiKeys from './apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+
+const getSpecies = (speciesId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/species.json?orderBy="speciesId"&equalTo="${speciesId}"`)
+    .then((response) => {
+      const demSpecies = response.data;
+      const species = [];
+      Object.keys(demSpecies).forEach((fbId) => {
+        demSpecies[fbId].id = fbId;
+        species.push(demSpecies[fbId]);
+      });
+      resolve(species);
+    })
+    .catch((error) => reject(error));
+});
+
+export default { getSpecies };
