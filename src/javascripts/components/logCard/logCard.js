d02a395774947acdd22041ecc26d198a36e2826a
Charity Bunyon <cseymou5@gmail.com>
2019-11-20 03:50:52 -0600

    added log board

--- src/javascripts/components/logCard/logCard.js
+++ src/javascripts/components/logCard/logCard.js
@@ -5,7 +5,7 @@ import utilities from '../../helpers/utilities';
 const makeLogBoard = () => {
   const domString = `
     <div class="card">
-      <div class="card-title text-center card-title"><h5>Meet The Crew</h5></div>
+      <div class="card-title text-center card-title"><h5>View Logs</h5></div>
       <img id="speciesImg" src="https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1266&q=80" class="card-img-top" alt="">
       <div class="card-body text-center">
       <a href="#" class="btn btn-danger btn-lg logHomeBtn">View</a>
