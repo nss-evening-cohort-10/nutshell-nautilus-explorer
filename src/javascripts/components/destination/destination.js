9778dc31ff982fede7b920ee20cad8f70f7a5d34
alesha reed <aleshak83@gmail.com>
2019-11-19 19:37:11 -0600

    added delete

--- src/javascripts/components/destination/destination.js
+++ src/javascripts/components/destination/destination.js
@@ -2,11 +2,27 @@ import $ from 'jquery';
 import './destination.scss';
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import getDestination from '../../helpers/data/destinationData';
+import destinationdata from '../../helpers/data/destinationData';
 import utilities from '../../helpers/utilities';
 
+
+const deleteDestinationbyId = (e) => {
+  e.preventDefault();
+  const id = e.target.id.split('delete-')[1];
+  console.log(e.target.id);
+  destinationdata.deleteDestination(id)
+    .then(() => {
+      // eslint-disable-next-line no-use-before-define
+      destinationBuilderAll();
+      // eslint-disable-next-line no-use-before-define
+      destinationLoginStatus();
+    })
+    .catch((error) => console.error(error));
+};
+
+
 const destinationBuilderAll = () => {
-  getDestination.getDestinations()
+  destinationdata.getDestinations()
     .then((destinations) => {
       let domString = `<h1 class="text-center">Destinations</h1>
         <table class="table table-striped">
@@ -26,23 +42,25 @@ const destinationBuilderAll = () => {
       <td>${destination.description}</td>
       <td><a href=${destination.destinationLink}">${destination.name} Links</a></td>
       <td><button type="link" class="btn btn-ink hide edit-destination" id="edit-${destination.id}">EDIT</button> 
-        <button type="link" class="btn btn-link hide delete-destination" id="delete-${destination.id}">DELETE</button></td>
+        <button type="link" class="btn btn-link hide deletes-destination" id="delete-${destination.id}">DELETE</button></td>
     </tr>`;
       });
       domString += '</tbody></table>';
       utilities.printToDom('destinationHome', domString);
+      $('#destinationHome').on('click', '.deletes-destination', deleteDestinationbyId);
     })
     .catch((error) => console.error(error));
 };
+
 
 const destinationLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       $('.edit-destination').removeClass('hide');
-      $('.delete-destination').removeClass('hide');
+      $('.deletes-destination').removeClass('hide');
     } else {
       $('.edit-destination').addClass('hide');
-      $('.delete-destination').addClass('hide');
+      $('.deletes-destination').addClass('hide');
     }
   });
   destinationBuilderAll();
