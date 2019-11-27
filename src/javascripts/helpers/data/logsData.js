fc976cc4ba30fe4e008d61bf466174162eb12e1b
Denise Baker <denviol@yahoo.com>
2019-11-26 19:41:06 -0600

    add logsData.js

---
+++ src/javascripts/helpers/data/logsData.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getLogs = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/logs.json`)
+    .then((response) => {
+      const demLogs = response.data;
+      const logs = [];
+      Object.keys(demLogs).forEach((fbId) => {
+        demLogs[fbId].id = fbId;
+        logs.push(demLogs[fbId]);
+      });
+      resolve(logs);
+    })
+    .catch((error) => reject(error));
+});
+
+
+export default { getLogs };
