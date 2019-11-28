06f874d7450287f7d79fe6d761f765e8257f5cce
Ivan Phelps <ivanniothemannio@gmail.com>
2019-11-28 17:37:57 -0600

    added environment name pair, radio button printing to add modal

--- src/javascripts/components/species/species.js
+++ src/javascripts/components/species/species.js
@@ -4,7 +4,9 @@ import $ from 'jquery';
 import util from '../../helpers/utilities';
 import speciesData from '../../helpers/data/speciesData';
 import speciesCard from '../Species Card/speciesCard';
+import enviData from '../../helpers/data/environmentData';
 import './species.scss';
+
 
 const deleteASpecies = (e) => {
   e.preventDefault();
@@ -15,6 +17,22 @@ const deleteASpecies = (e) => {
       buildSpecies();
     })
     .catch((error) => console.error(error));
+};
+
+const populateModalRadios = () => {
+  enviData.getEnvis()
+    .then((environments) => {
+      let domString = '<h5>Select Environment</h5>';
+      environments.forEach((environment) => {
+        domString += `<div class="environment-radios">
+        <input class="form-check-input" type="radio" name="exampleRadios" id="${environment.name}-radio" value="${environment.id}">
+        <label class="form-check-label" for="exampleRadios1">
+          ${environment.name}
+          </label>
+          </div>`;
+      });
+      util.printToDom('species-environment-radio', domString);
+    });
 };
 
 const addASpecies = (e) => {
@@ -79,7 +97,7 @@ const buildSpecies = () => {
       let domString = '';
       if (userSignedIn) {
         domString = '<h1 class="add-a-species text-center board-header">View Species</h1>';
-        domString += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addSpeciesModal">Add Species</button>';
+        domString += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addSpeciesModal" id="add-species-button">Add Species</button>';
         domString += '<div id="species-section" class="d-flex flex-wrap justify-content-center">';
       } else {
         domString = '<h1 class="add-a-species text-center board-header">View Species</h1>';
@@ -96,6 +114,7 @@ const buildSpecies = () => {
           $(document.body).on('click', '#add-new-species', addASpecies);
           $('.update-species-modal').click(getSpeciesModal);
           $('.update-species').click(updateASpecies);
+          $('#add-species-button').click(populateModalRadios);
         });
     })
     .catch((error) => console.error(error));
