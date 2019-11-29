8457f265cadf24df76940fdfa9cb411d953b19bb
Denise Baker <denviol@yahoo.com>
2019-11-29 17:21:49 -0600

    added excursion seed data

---
+++ src/javascripts/helpers/data/excursionsData.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getExcursions = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/excursions.json`)
+    .then((response) => {
+      const demExcursions = response.data;
+      const excursions = [];
+      Object.keys(demExcursions).forEach((fbId) => {
+        demExcursions[fbId].id = fbId;
+        excursions.push(demExcursions[fbId]);
+      });
+      resolve(excursions);
+    })
+    .catch((error) => reject(error));
+});
+
+const deleteExcursion = (excursionId) => axios.delete(`${baseUrl}/excursions/${excursionId}.json`);
+const addNewExcursion = (newExcursion) => axios.post(`${baseUrl}/excursions.json`, newExcursion);
+
+export default { getExcursions, deleteExcursion, addNewExcursion };
